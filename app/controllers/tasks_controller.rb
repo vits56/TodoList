0b8e54f4f00d275e4fdad8c6dbe3a309b2dccf00
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-06-06 02:18:25 -0300

    lista de tarefas

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+        format.html { redirect_to tasks_url, notice: "Task was successfully created." }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
